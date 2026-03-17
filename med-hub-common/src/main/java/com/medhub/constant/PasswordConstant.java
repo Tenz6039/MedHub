6112e4a0d95b7c85eb7526891550818f8f1b0d0a
tenz6039 <603930987@qq.com>
2026-03-18 01:04:57 +0800

    项目初始化

---
+++ med-hub-common/src/main/java/com/medhub/constant/PasswordConstant.java
@@ -0,0 +1,13 @@
+package com.medhub.constant;
+
+/**
+ * 密码常量
+ */
+public class PasswordConstant {
+
+    /**
+     * 默认密码
+     */
+    public static final String DEFAULT_PASSWORD = "123456";
+
+}
